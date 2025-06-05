23809e2acc9dbd6a61887ad496d290358848ed74
Brandon Edwards <b_edwards@ducks.ca>
2025-06-05 14:49:38 -0700

    start quarto report and provide summar statistics and plots

--- _targets.R
+++ _targets.R
@@ -6,6 +6,10 @@
 
 # Load packages required to define the pipeline:
 library(targets)
+library(tarchetypes)
+library(ggplot2)
+library(ggpubr)
+theme_set(theme_pubclean())
 
 # Set target options:
 tar_option_set(
@@ -45,18 +49,72 @@ list(
     command = read.csv(cwi_impact_wsa_data_file)
   ),
   
-  # Targets for data wrangling
+  # Targets for exploratory plots of data
   tar_target(
-    name = drained_vb,
-    command = merge(x = cwi_impact_vb_data[which(cwi_impact_vb_data$CWI_Impact %in% c(1,5)),], 
-                    y = drains_vb_data[, c("BasinID", "Drains_km", "Drains_count")],
-                    by = "BasinID")
+    name = drain_length_plot,
+    command = ggplot(data = drains_vb_data[which(drains_vb_data$Drains_km > 0), ], 
+                     aes(x = Drains_km)) + 
+                geom_histogram() +
+                xlab("KM of Drains") +
+                ylab("Count") + 
+                NULL
+  ),
+  tar_target(
+    name = n_drains_plot,
+    command = ggplot(data = drains_vb_data[which(drains_vb_data$Drains_count > 0),], 
+                     aes(x = Drains_count)) +
+                geom_histogram() +
+                xlab("Number of Drains") +
+                ylab("Count") +
+                NULL
+  ),
+  tar_target(
+    name = length_vs_n_plot,
+    command = ggplot(data = drains_vb_data[which(drains_vb_data$Drains_count > 0 &
+                                                 drains_vb_data$Drains_km > 0), ],
+                    aes(x = Drains_count, y = Drains_km)) +
+                geom_point() +
+                xlab("Number of Drains") +
+                ylab("Length of Drains") +
+                NULL
+  ),
+  tar_target(
+    name = combined_drains_plot,
+    command = ggarrange(ggarrange(drain_length_plot, n_drains_plot),
+                        length_vs_n_plot,
+                        nrow = 2)
   ),
   
-  # Targets for modelling
   tar_target(
-    name = drained_vb_model,
-    command = model_drained_vb()
+    name = class_1_drainage_plot,
+    command = ggplot(data = cwi_impact_vb_data[which(cwi_impact_vb_data$CWI_Impact == 1), ],
+                      aes(x = Area_km2 / WS_AREA_KM2)) +
+                geom_histogram() +
+                xlab("Percent Drained Class 1") +
+                ylab("Count") +
+                NULL
+  ),
+  tar_target(
+    name = class_5_drainage_plot,
+    command = ggplot(data = cwi_impact_vb_data[which(cwi_impact_vb_data$CWI_Impact == 5), ],
+                      aes(x = Area_km2 / WS_AREA_KM2)) +
+                geom_histogram() +
+                xlab("Percent Drained Class 5") +
+                ylab("Count") +
+                NULL
+  ),
+  tar_target(
+   name = combined_class_drainage_plot,
+   command = ggarrange(class_1_drainage_plot, class_5_drainage_plot)
+  ),
+  
+  
+  
+  
+  # Final report
+  tar_quarto(
+    name = report,
+    path = "output/reports/report.qmd"
   )
   
 )
